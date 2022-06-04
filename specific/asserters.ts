cb8a713e3f5c23c9f732746b85b0755e53d5ff79
Anton Ebbage <anton.ebbage@outlook.com>
2022-06-04 20:07:30 +0100

    docs: edit `Asserter` JSDoc comments
    [skip ci]

--- specific/asserters.ts
+++ specific/asserters.ts
@@ -2,15 +2,15 @@
 
 import { type } from '../generic/mod.ts';
 
-/** `_boolean` is a `boolean` assertion function. If `value` is a `boolean`,
- * `_boolean` returns `value` as `boolean`. Otherwise, `_boolean` throws a
- * `TypeAssertionError`, including `name` in its `message`. */
+/** `_boolean` is a `boolean` type assertion function. If `value` is a
+ * `boolean`, `_boolean` returns `value` as `boolean`. Otherwise, `_boolean`
+ * throws a `TypeAssertionError`, including `name` in its `message`. */
 export const _boolean = type(
   'boolean',
   (value): value is boolean => typeof value === 'boolean',
 );
 
-/** `_number` is a `number` assertion function. If `value` is a `number`,
+/** `_number` is a `number` type assertion function. If `value` is a `number`,
  * `_number` returns `value` as `number`. Otherwise, `_number` throws a
  * `TypeAssertionError`, including `name` in its `message`. */
 export const _number = type(
@@ -18,7 +18,7 @@ export const _number = type(
   (value): value is number => typeof value === 'number',
 );
 
-/** `_string` is a `string` assertion function. If `value` is a `string`,
+/** `_string` is a `string` type assertion function. If `value` is a `string`,
  * `_string` returns `value` as `string`. Otherwise, `_string` throws a
  * `TypeAssertionError`, including `name` in its `message`. */
 export const _string = type(
