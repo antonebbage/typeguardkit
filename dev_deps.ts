d50a0c404a9baa40101c3756a28ce3a50848ed04
Anton Ebbage <anton.ebbage@outlook.com>
2022-07-09 09:21:20 +0100

    build: update dev deps

--- dev_deps.ts
+++ dev_deps.ts
@@ -3,5 +3,5 @@ export {
   assertStrictEquals,
   assertStringIncludes,
   assertThrows,
-} from "https://deno.land/std@0.146.0/testing/asserts.ts";
-export { describe, it } from "https://deno.land/std@0.146.0/testing/bdd.ts";
+} from "https://deno.land/std@0.147.0/testing/asserts.ts";
+export { describe, it } from "https://deno.land/std@0.147.0/testing/bdd.ts";
