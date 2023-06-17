ca4ea4514aba840741ccef437d0157222299c0e1
Anton Ebbage <anton.ebbage@outlook.com>
2023-06-17 14:06:26 +0100

    refactor: move `_undefined` to `/asserters`

--- asserters/undefined.ts
+++ asserters/undefined.ts
@@ -1,3 +1,14 @@
 // This module is browser-compatible.
 
-export { _undefined } from "../core/undefined.ts";
+import { typeAsserter } from "../core/mod.ts";
+
+/**
+ * `_undefined` is an `undefined` type assertion function. If `value` is
+ * `undefined`, `_undefined` returns `value` as `undefined`. Otherwise,
+ * `_undefined` throws a `TypeAssertionError`, including `valueName` in its
+ * `message`.
+ */
+export const _undefined = typeAsserter(
+  "undefined",
+  (value): value is undefined => value === undefined,
+);
